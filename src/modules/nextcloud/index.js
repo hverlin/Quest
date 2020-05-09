a6f3ab4be66a208ba2c3b604634e1c396bd466f1
Antoine Chabert <antoine.chabert@tohero.fr>
2020-05-09 11:19:10 +0200

    Add detail and update API
    Signed-off-by: Antoine Chabert <antoine.chabert@tohero.fr>

--- src/modules/nextcloud/index.js
+++ src/modules/nextcloud/index.js
@@ -5,6 +5,28 @@ import { ExternalLink } from "../../components/external-link";
 import logo from "./logo.svg";
 import qs from "qs";
 import { PaginatedResults } from "../../components/paginated-results/paginated-results";
+import { H3 } from "@blueprintjs/core";
+import { Size } from "../../components/size";
+
+function NextcloudDetail({ item, url }) {
+  const { name, link, modified, type, path, mime_type, size } = item;
+
+  return (
+    <div>
+      <div>
+        <H3>
+          <ExternalLink href={url + link}>
+            {type} - {name}
+          </ExternalLink>
+        </H3>
+      </div>
+      <p>
+        Updated: <Time seconds={modified} /> | Path: {path} | Mime-Type: {mime_type} | Size:{" "}
+        <Size bytes={size} />
+      </p>
+    </div>
+  );
+}
 
 function NextcloudResultItem({ item = {}, url }) {
   const { name, link, modified, type } = item;
@@ -12,7 +34,7 @@ function NextcloudResultItem({ item = {}, url }) {
   return (
     <>
       <p>
-        <ExternalLink href={url + link.replace(/\\/g, "")}>
+        <ExternalLink href={url + link}>
           {type} - {name}
         </ExternalLink>
       </p>
@@ -25,8 +47,8 @@ function NextcloudResultItem({ item = {}, url }) {
 
 const makeNextcloudRenderer = (url) => ({ pages }) => {
   return _.flatten(
-    pages.map(({ result }) => {
-      return result?.map((item) => ({
+    pages.map(({ results }) => {
+      return results?.map((item) => ({
         key: item.id,
         component: <NextcloudResultItem key={item.id} url={url} item={item} />,
         item: item,
@@ -38,9 +60,9 @@ const makeNextcloudRenderer = (url) => ({ pages }) => {
 async function nextcloudResultsFetcher(
   key,
   { input, pageSize, username, password, baseUrl },
-  offset
+  page = 1
 ) {
-  const searchParams = qs.stringify({ q: input, page: offset || 1, limit: pageSize });
+  const searchParams = qs.stringify({ q: input, page, limit: pageSize, computeHasMore: true });
 
   const url = `${baseUrl}/index.php/apps/quest/api/0.1/search?${searchParams}`;
 
@@ -56,9 +78,8 @@ async function nextcloudResultsFetcher(
   const result = await res.json();
 
   return {
-    result,
-    total: result.length,
-    offset: offset || 1,
+    ...result,
+    page,
   };
 }
 
@@ -76,12 +97,9 @@ export default function NextcloudSearchResults({ configuration, searchViewState 
       logo={logo}
       globalError={!url ? "Nextcloud module is not configured correctly. URL is missing." : null}
       configuration={configuration}
-      getFetchMore={({ total, offset }) => (total >= pageSize ? offset + 1 : null)}
+      getFetchMore={({ has_more, page }) => (has_more ? page + 1 : null)}
       fetcher={nextcloudResultsFetcher}
-      itemDetailRenderer={() => {
-        //TODO complete this part
-        return null;
-      }}
+      itemDetailRenderer={(item) => <NextcloudDetail item={item} url={url} />}
       renderPages={makeNextcloudRenderer(url)}
     />
   );
